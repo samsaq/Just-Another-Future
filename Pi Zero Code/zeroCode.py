043b1a8a3acd18b12fc61b077024031b4b727a4d
samsaq <sameereza@gmail.com>
2023-04-21 15:56:16 +0800

    added folder structure for pi

---
+++ Pi Zero Code/zeroCode.py
@@ -0,0 +1,8 @@
+# this file will be the code that runs on the zero to await requests made by
+# the website & apply for & then resolve those prompts
+# should display a loading screen while waiting for an image
+# and after the first prompted image is displayed, fade between the generated variants
+# do so until the screen is touched or X time passes
+# should be at an intro screen of some kind while awaiting a new prompt
+import requests
+
